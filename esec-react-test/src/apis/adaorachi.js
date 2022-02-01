5dbe23848bc7c9d01ffed531d5d7a40859c06bf5
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2022-02-01 10:26:06 +0100

    first commit

---
+++ esec-react-test/src/apis/adaorachi.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "https://adaorachi.github.io/esetech-assessment-api/game-data.json",
+});
---
+++ esec-react-test/src/components/VideoCards.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function VideoCards() {
+  return <div></div>;
+}
+
+export default VideoCards;
